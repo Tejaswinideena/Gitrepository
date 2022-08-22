df8e0cb8b094c62b815914e43b3682e047e49407
Tejaswinideena <111355809+Tejaswinideena@users.noreply.github.com>
2022-08-22 19:08:01 +0530

    Delete Program6.cs

--- Hands-On/Day 3(22 Aug)/Program6.cs
+++
@@ -1,13 +0,0 @@
-﻿using System;using System.Collections.Generic;public class Program6{        public static void Main(string[] args)        {            Stack<int> st = new Stack<int>();
-
-            st.Push(90);            st.Push(20);            st.Push(30);            st.Push(50);            st.Push(35);                        Console.WriteLine("Stack Elements Before Pop");            foreach (var item in st)            {                Console.WriteLine(item);            }            st.Pop();
-            Console.WriteLine("\nStack Elements After Pop");
-            foreach (var item in st)
-            {
-                Console.WriteLine(item);
-            }
-            
-            Console.WriteLine("\ncount of elements in stack: " + st.Count);
-            Console.WriteLine("\ntop element: "+st.Peek());
-             
-            Console.ReadLine();        }}
