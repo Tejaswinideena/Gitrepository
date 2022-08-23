75c9d732af5e203b2dd23dfe626c0804d93d7f20
Tejaswinideena <111355809+Tejaswinideena@users.noreply.github.com>
2022-08-23 12:07:01 +0530

    Add files via upload

---
+++ Hands-On/Day 2(19 Aug)/Program15.cs
@@ -0,0 +1,20 @@
+﻿using System;
+public class Program15
+{
+        public static void Main(string[] args)
+        {
+            int i = 0, j = 25;
+
+            for (i = 0, j = 25; i < 25 && j >= 0; i++, j--)
+            {
+                if (i > j)
+                {
+                    Console.WriteLine("\nCrossed Over");
+                    break;
+                }
+                Console.WriteLine("{0} {1}", i, j);
+        }
+            Console.ReadLine();
+        }
+    
+}
---
+++ Hands-On/Day 2(19 Aug)/Program16.cs
@@ -0,0 +1,27 @@
+﻿using System;
+public class Program16
+{
+        public static void Main(string[] args)
+        {
+            int i;
+            string str = " ";
+            int sum = 100;
+            for (i = 0; i < 4; i++)
+            {
+                str = Console.ReadLine();
+                if (str.Equals("hit"))
+                {
+                    sum += 10;
+                }
+                else if (str.Equals("miss"))
+                {
+                    sum -= 20;
+                }
+            }
+            Console.WriteLine("\n");
+            Console.WriteLine(sum);
+            Console.ReadLine();
+
+        }
+    
+}
