d87623240897002389f2df264a0e1f8de3762af8
Tejaswinideena <111355809+Tejaswinideena@users.noreply.github.com>
2022-08-24 18:57:41 +0530

    Main file where methods are called
    Today's Assignment is to work with class library,where we call our methods in main project file.
    Simple programs are given inorder to practice this way of working.

---
+++ Hands-On/Day 5(24 Aug)/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ReusableCodeLibrary;
+
+namespace MyApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            StringOperations.Compare();
+            StringOperations.Copy();
+            StringOperations.Indexof();
+            StringOperations.Split();
+            StringOperations.Uppercase();
+            FileandDate.Datedetails();
+            FileandDate.Makefile();
+           
+        }
+    }
+}
