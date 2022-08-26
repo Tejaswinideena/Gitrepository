2c809f12c97ac0f096ead5943ca66b9a5da54288
Tejaswinideena <111355809+Tejaswinideena@users.noreply.github.com>
2022-08-26 08:53:38 +0530

    Add files via upload

---
+++ Hands-On/Day 6(25 Aug)/Program1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+using System.Security.Permissions;
+
+public class Program1
+{
+        public static void Main(string[] args)
+        {
+            
+                    Console.WriteLine("1.create\n2.add\n3.truncate\n");
+                    int x = Convert.ToInt32(Console.ReadLine());
+                    string path = "E:\\FilesDemo\\";
+                    Console.WriteLine("Enter the file name");
+                    string fname = Console.ReadLine();
+                    fname = string.Concat(path, fname);
+
+                    switch (x)
+                    {
+                        case 1:
+
+                            FileStream fs = new FileStream(fname, FileMode.Create,FileAccess.Write);
+                            StreamWriter sw = new StreamWriter(fs);
+
+                            sw.WriteLine("This is a new file created using Create method.");
+                            sw.Close();
+                            fs.Close();
+                            Console.WriteLine("Created successfully...");
+                            break;
+
+                        case 2:
+
+                            FileStream fs1 = new FileStream(fname, FileMode.Append,FileAccess.Write);
+                            StreamWriter sw1 = new StreamWriter(fs1);
+                            sw1.WriteLine("Add this Contents into existing file.");
+                            sw1.Close();
+                            fs1.Close();
+                            Console.WriteLine("Appended successfully...");
+                            break;
+                        case 3:
+
+                            FileStream fs2 = new FileStream(fname, FileMode.Truncate, FileAccess.Write);
+                            fs2.Close();
+                            Console.WriteLine("Truncated successfully...");
+                            break;
+
+
+
+                    }
+     
+
+            
+        }
+}
---
+++ Hands-On/Day 6(25 Aug)/Program4.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+public class Program4
+{
+    public static void Main()
+    {   
+        DriveInfo[] drivelist = DriveInfo.GetDrives();
+        Console.WriteLine("List of All Drives\n");
+        foreach (var item in drivelist)
+        {
+            Console.WriteLine("Drive {0}",item.Name);
+        }
+        Console.WriteLine("\nC Drive Information");
+        DriveInfo info = new DriveInfo("C");
+        Console.WriteLine("Name: " +info.Name);
+        Console.WriteLine("Totalsize: "+info.TotalSize);
+        Console.WriteLine("Type: "+info.DriveType);
+        Console.WriteLine("Format: "+info.DriveFormat);
+
+        Console.ReadLine();
+    }
+}
