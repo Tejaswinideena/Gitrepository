18c7635f28d221e78592ba370c30527387e060ae
Tejaswinideena <111355809+Tejaswinideena@users.noreply.github.com>
2022-08-26 08:56:39 +0530

    Add files via upload

--- Hands-On/Day 6(25 Aug)/Program2.cs
+++ Hands-On/Day 6(25 Aug)/Program2.cs
@@ -7,31 +7,23 @@ public class Program2
 {
     public static void Main(string[] args)
     {
-        //string path = "E:\\FilesDemo\\";
-        //Console.WriteLine("Enter the file name");
-        //string fname = Console.ReadLine();
-        //fname = string.Concat(path, fname);
-        //File.Create(fname);
-        //Console.WriteLine("Created Successfully");
-        //string str = "This is new file";
-        //File.WriteAllText(fname, str);
-        //Console.WriteLine("Added Successfully");
-        //File.Copy(fname,"copyhello.txt");
-        //File.Move(fname,"movehello.txt");
-        //File.Delete(fname);
-        //FileInfo fileInfo = new FileInfo(fname);
-        //Console.WriteLine("Extension: "+fileInfo.Extension);
-        //Console.WriteLine("DirectoryName: "+fileInfo.DirectoryName);
-        //Console.WriteLine("TimeOfCreation: "+fileInfo.CreationTime);
-        //Directory.CreateDirectory("Results");
-        string Results= "E:\\FilesDemo\\Results";
-        //string Results17_18= "E:\\FilesDemo\\Results\\";
-        //if (!Directory.Exists(Results))
-        //{
-        //    Directory.CreateDirectory(Results);
-        //}
-        
-        Directory.CreateDirectory(Path.Combine(Results,"Results17-18!"));
+        string path = "E:\\FilesDemo\\";
+        Console.WriteLine("Enter the file name");
+        string fname = Console.ReadLine();
+        fname = string.Concat(path, fname);
+        File.Create(fname);
+        Console.WriteLine("Created Successfully");
+        string str = "This is new file";
+        File.WriteAllText(fname, str);
+        Console.WriteLine("Added Successfully");
+        File.Copy(fname,"copyhello.txt");
+        File.Move(fname,"movehello.txt");
+        File.Delete(fname);
+        FileInfo fileInfo = new FileInfo(fname);
+        Console.WriteLine("Extension: "+fileInfo.Extension);
+        Console.WriteLine("DirectoryName: "+fileInfo.DirectoryName);
+        Console.WriteLine("TimeOfCreation: "+fileInfo.CreationTime);
+       
         Console.ReadLine();
 
     }
