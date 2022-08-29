7a9db2737f61cfb02bd41f7145beb6031fff1bb7
Tejaswinideena <111355809+Tejaswinideena@users.noreply.github.com>
2022-08-29 18:00:37 +0530

    Add files via upload

---
+++ Hands-On/Day 8(29 Aug)/Program1(a).cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program
+{
+    public class Program1a
+    {
+
+        public static void Main(string[] args)
+        {
+            //Linked List using generic classes
+            GenericsLinkedList<string> str = new GenericsLinkedList<string>(); 
+            string s1 = "Welcome";
+            string s2 = "to";
+            string s3 = "C Sharp";
+            string s4 = "Training";
+            str.AddHead(s1);
+            str.AddHead(s2);
+            str.AddHead(s3);
+            str.AddHead(s4);
+            foreach (string Str in str)
+            {
+                Console.WriteLine(Str);
+            }
+            Console.ReadKey();
+        }
+    }
+
+    class GenericsLinkedList<T>
+    {
+        class LinkedlistNode
+        {
+            private LinkedlistNode next;
+            private T item;
+
+            public LinkedlistNode(T t)
+            {
+                next = null;
+                item = t;
+
+            }
+            public LinkedlistNode Next
+            {
+                get
+                {
+                    return next;
+                }
+                set
+                {
+                    next = value;
+                }
+            }
+            public T Item
+            {
+                get
+                {
+                    return item;
+                }
+                set
+                {
+                    item = value;
+                }
+            }
+        }
+        private LinkedlistNode head;
+        public GenericsLinkedList()
+        {
+            head = null;
+        }
+        public void AddHead(T t)
+        {
+            LinkedlistNode node = new LinkedlistNode(t);
+            node.Next = head;
+            head = node;
+        }
+        public IEnumerator<T> GetEnumerator()
+        {
+            LinkedlistNode current = head;
+            while (current != null)
+            {
+                yield return current.Item;
+                current = current.Next;
+            }
+
+        }
+    }
+}
---
+++ Hands-On/Day 8(29 Aug)/Program1(b).cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Program
+{
+    public class Program1b
+    {
+        public static void Main(string[] args)
+        {
+            //Stack using generic classes
+            Userstack<string> u = new Userstack<string>();
+            u.Push("Raj");
+            u.Push("Lakshman");
+            u.Push("Ram");
+            u.Push("Sita");
+            u.Push("Nina");
+            Console.WriteLine("Elements at Stack Before Pop: ");    
+            u.printData();
+            Console.WriteLine("\nPopped element");
+            u.Pop();
+            Console.WriteLine("\nElements at Stack After Pop: ");
+            u.printData();
+            Console.WriteLine("\nPeek element: ");
+            string k = u.Peek();
+            Console.WriteLine(k);
+            Console.ReadLine();
+
+
+        }
+
+    }
+    class Userstack<T>
+    {
+        internal static int max = 10;
+        T[] stack = new T[max];
+        int top = -1;
+        public void Push(T str)
+        {
+            if (top >= 10)
+            {
+                Console.WriteLine("No space on stack");
+            }
+            else
+            {
+                stack[++top] = str;
+            }
+        }
+        public void Pop()
+        {
+            if (top < 0)
+            {
+                Console.WriteLine("No elements on stack");
+            }
+            else
+            {
+                T value = stack[top--];
+                Console.WriteLine(value);
+            }
+        }
+        public T Peek()
+        {
+            if (top < 0)
+            {
+                return default(T);
+            }
+            else
+            {
+                T value = stack[top];
+                return value;
+            }
+        }
+        public void printData()
+        {
+            for (int i = top; i >= 0; i--)
+            {
+                Console.WriteLine(stack[i]);
+            }
+        }
+    }
+}
