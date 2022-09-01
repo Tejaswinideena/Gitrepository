1ee501f9f138e0337bea44553fb040fe736dbf5b
Tejaswinideena <111355809+Tejaswinideena@users.noreply.github.com>
2022-09-01 15:13:20 +0530

    Add files via upload

---
+++ Hands-On/Day 10(1 Sept)/MergeSort(2ndway).cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program
+{
+    public class MergeSort_2
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[10] { 32, 21, 15, 57, 65, 4, 62, 7, 40, 82 };
+            Console.WriteLine("Before Sort: ");
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+            sort(arr, 0, arr.Length - 1);
+
+            Console.WriteLine("\nAfter sort: ");
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+     
+            Console.ReadLine();
+
+        }
+        static void sort(int[] arr, int low, int high)
+        {
+            int mid;
+            if (low < high)
+            {
+                mid = (low + high) / 2;
+                sort(arr, low, mid);
+                sort(arr, mid + 1, high);
+                merging(arr, low, mid, high);
+
+            }
+            else
+            {
+                return;
+            }
+
+        }
+
+        private static void merging(int[] arr, int low, int mid, int high)
+        {
+            int[] barr = new int[10];
+            int l1 = 0, l2 = 0, i;
+            for (l1 = low, l2 = mid + 1, i = low; l1 <= mid && l2 <= high; i++)
+            {
+
+                if (arr[l1] <= arr[l2])
+                {
+                    barr[i] = arr[l1++];
+                }
+                else
+                {
+                    barr[i] = arr[l2++];
+                }
+
+            }
+            while (l1 <= mid)
+
+                barr[i++] = arr[l1++];
+
+            while (l2 <= high)
+
+                barr[i++] = arr[l2++];
+
+            for (i = low; i <= high; i++)
+
+                arr[i] = barr[i];
+
+        }
+    }
+}
---
+++ Hands-On/Day 10(1 Sept)/ShellSort.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program
+{
+    public class ShellSort
+    {
+        static void Main(string[] args)
+        {
+            Shellsort();
+            Console.Read();
+        }
+        static void printArr(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+
+        }
+
+        static void Shellsort()
+        {
+            int[] arr = new int[] { 25, 13, 43, 30, 94, 37, 87, 14, 74 };
+            int inner, outer, valueToInsert, interval = 1, i = 0;
+            Console.WriteLine("Before Sort: ");
+            printArr(arr);
+            Console.WriteLine("----------------------------");
+            while (interval <= ((arr.Length) / 3))
+            {
+                interval = interval * 3 + 1;
+            }
+            while (interval > 0)
+            {
+                for (outer = 0; outer < arr.Length; outer++)
+                {
+                    valueToInsert = arr[outer];
+                    inner = outer;
+                    while ((inner > interval - 1) && (arr[inner - interval] >= valueToInsert))
+                    {
+                        arr[inner] = arr[inner - interval];
+                        inner = inner - interval;
+                        Console.WriteLine("Item moved: " + arr[inner]);
+                    }
+                    arr[inner] = valueToInsert;
+                    Console.WriteLine("Item inserted: " + arr[inner]);
+                }
+                interval = (interval - 1) / 3;
+                i += 1;
+            }
+            Console.WriteLine("----------------------------");
+            Console.WriteLine("After Sort: ");
+            printArr(arr);
+
+        }
+    }
+}
