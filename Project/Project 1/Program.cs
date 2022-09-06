9f0a4d2c0eeda5d4ea482b7a366fc1c8571bf267
Tejaswinideena <111355809+Tejaswinideena@users.noreply.github.com>
2022-09-06 17:28:28 +0530

    Retrieve Student Data from a Text File

---
+++ Project/Project 1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.IO;
+namespace MyProject
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string path = "E:\\Project\\Project 1\\Student Details.txt";
+            string studentfile=File.ReadAllText(path);
+            Console.WriteLine(studentfile);
+        }
+    }
+}
