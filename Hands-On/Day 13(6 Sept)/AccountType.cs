9aa3d001dbf8f857c08ce8dba864139d146f131a
Tejaswinideena <111355809+Tejaswinideena@users.noreply.github.com>
2022-09-07 09:15:35 +0530

    Add files via upload

---
+++ Hands-On/Day 13(6 Sept)/AccountType.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReusableCodeLibrary
+{
+    public class AccountType
+    {
+        internal Type TypeOfAccount { get; set; }
+        public void ShowMenu()
+        {
+            Console.WriteLine("Enter account type \n1 for savings \n2 for current");
+            TypeOfAccount = (Type)Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Selected account type= " + TypeOfAccount);
+            Savings s = new Savings();
+            
+        }
+        internal enum Type
+        {
+            Savings=1,
+            Current=2
+        }
+        
+    }
+
+        public class Savings
+        {
+            public void CalculateInterest()
+            {
+                Console.WriteLine("Interest for Savings Account is 4%");
+            }
+        
+        
+        }
+        public class Current
+        {
+            public void CalculateInterest()
+            {
+                Console.WriteLine("Interest for Current Account is 2%");
+            }
+        }
+    
+}
---
+++ Hands-On/Day 13(6 Sept)/ExtensionMethod.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReusableCodeLibrary
+{
+    public static class ExtensionMethod
+    {
+        static int AvailableBalance = 10000;
+        public static bool Withdraw(this Savings s)
+        {
+            Console.WriteLine("Enter the amt to Withdraw");
+            int amt = Convert.ToInt32(Console.ReadLine());
+
+            bool status = false;
+            if (amt <= AvailableBalance)
+            {
+                AvailableBalance = AvailableBalance - amt;
+                status = true;
+            }
+            Console.WriteLine("After withdraw available bal=" + AvailableBalance);
+
+            return status;
+        }
+        public static bool Deposit(this Savings s)
+        {
+            Console.WriteLine("Enter the amt to Deposit");
+            int amt = Convert.ToInt32(Console.ReadLine());
+
+            bool status = false;
+            if (amt <= AvailableBalance)
+            {
+                AvailableBalance = AvailableBalance + amt;
+                status = true;
+            }
+            Console.WriteLine("After Deposit available bal=" + AvailableBalance);
+
+            return status;
+        }
+        public static bool Withdraw(this Current c)
+        {
+            Console.WriteLine("Enter the amt to Withdraw");
+            int amt = Convert.ToInt32(Console.ReadLine());
+
+            bool status = false;
+            if (amt <= AvailableBalance)
+            {
+                AvailableBalance = AvailableBalance - amt;
+                status = true;
+            }
+            Console.WriteLine("After withdraw available bal=" + AvailableBalance);
+
+            return status;
+        }
+        public static bool Deposit(this Current c)
+        {
+            Console.WriteLine("Enter the amt to Deposit");
+            int amt = Convert.ToInt32(Console.ReadLine());
+
+            bool status = false;
+            if (amt <= AvailableBalance)
+            {
+                AvailableBalance = AvailableBalance + amt;
+                status = true;
+            }
+            Console.WriteLine("After Deposit available bal=" + AvailableBalance);
+
+            return status;
+        }
+        
+    }
+}
