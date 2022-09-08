0520a38fc6a64c80644f417b977d748580a4f080
Tejaswinideena <111355809+Tejaswinideena@users.noreply.github.com>
2022-09-08 10:23:06 +0530

    Add files via upload

---
+++ Hands-On/Day 14(7 Sept)/Delegate.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Handson
+{
+        public delegate void stringOp(string s);
+        public delegate void MathOp(int i, int j);
+        public delegate void Productdel(Products1 p);
+        public delegate int Joindate(DateTime t);
+        public delegate void Cap(Capitalize c);
+        public class stroperation
+        {
+            public void stringlen(string s)
+            {
+                Console.WriteLine(s.Length);
+
+            }
+            public void greet(string s)
+            {
+                Console.WriteLine("Hello "+s);
+            }
+        }
+        public class integer
+        {
+            public void Add(int i, int j)
+            {
+                Console.WriteLine($"Addition is { i + j}");
+            }
+            public void Sub(int i, int j)
+            {
+                if (i > j)
+                {
+                    Console.WriteLine($"Subtraction is {i - j}");
+                }
+                else
+                {
+                    Console.WriteLine($"Subtraction is {j - i}");
+                }
+            }
+            public void Multiply(int i, int j)
+            {
+                Console.WriteLine($"Multiplication is {j * i}");
+            }
+
+        }
+        public class Products1
+        {
+            public int PId
+            {
+                get;
+                set;
+            }
+            public string Name
+            {
+                get;
+                set;
+            }
+            public void Display(Products1 p)
+            {
+                Console.WriteLine("Product id is "+p.PId + " and Product Name is "+p.Name);
+            }
+        }
+        public class Employee
+        {
+            private DateTime _joiningdate;
+            public DateTime JoiningDate
+            {
+                get { return _joiningdate; }
+                set { _joiningdate = value; }
+
+            }
+            public int Expyrs(DateTime ji)
+            {
+                int p = DateTime.Now.Year - ji.Year;
+                return p;
+            }
+        }
+        public class Capitalize
+        {
+            public void FirstCharToUpper(Capitalize c)
+            {
+                string str = "first";
+
+                if (str.Length == 0)
+                {
+                    Console.WriteLine("Empty String");
+                }
+                else if (str.Length == 1)
+                {
+                    Console.WriteLine(char.ToUpper(str[0]));
+                }
+                else
+                {
+                    Console.WriteLine(char.ToUpper(str[0]) + str.Substring(1));
+                }
+            }
+        }
+    
+}
---
+++ Hands-On/Day 14(7 Sept)/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using Handson;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day14
+{
+    public class Program
+    {
+        public delegate string Concat(string i, string j);
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1.String Length");
+            Console.WriteLine("Enter the string");
+            string str = Console.ReadLine();
+            stroperation s1 = new stroperation();
+            stringOp s2 = new stringOp(s1.stringlen);
+            s2(str);
+
+            Console.WriteLine("\n2.Greet User");
+            Console.WriteLine("Enter username");
+            string greet1 = Console.ReadLine();
+            stringOp s3 = new stringOp(s1.greet);
+            s3(greet1);
+
+            Console.WriteLine("\n3.Math Operations");
+            Console.WriteLine("Enter the two numbers");
+            int x = Convert.ToInt32(Console.ReadLine());
+            int y = Convert.ToInt32(Console.ReadLine());
+            integer math= new integer();
+            MathOp[] d3 = new MathOp[3] { math.Add, math.Sub, math.Multiply };
+            MathOp d4 = (MathOp)MulticastDelegate.Combine(d3);
+            d4(x, y);
+
+            Console.WriteLine("\n4.Display Products");
+            Console.WriteLine("Enter productId and product name");
+            Products1 p1 = new Products1();
+            p1.PId = Convert.ToInt32(Console.ReadLine());
+            p1.Name = Console.ReadLine();
+            Productdel prod = new Productdel(p1.Display);
+            prod(p1);
+
+            Console.WriteLine("\n5.Calculate experience year");
+            Console.WriteLine("Enter the year,Month,date");
+            int yr = Convert.ToInt32(Console.ReadLine());
+            int month = Convert.ToInt32(Console.ReadLine());
+            int dt = Convert.ToInt32(Console.ReadLine());
+            Employee emp = new Employee();
+            DateTime t = new DateTime(yr, month, dt);
+            emp.JoiningDate = t;
+            Joindate s4 = new Joindate(emp.Expyrs);
+            int exp = s4(emp.JoiningDate);
+            Console.WriteLine("Years of experience is "+exp);
+
+            Console.WriteLine("\n6.Concatenate two strings");
+            Console.WriteLine("Enter strings to Concatenate");
+            string m = Console.ReadLine();
+            string n = Console.ReadLine();
+            Concat con = delegate (string sm, string sn)
+            {
+                return sm + sn;
+            };
+            string str1 = con(m, n);
+            Console.WriteLine("Concatenated string is "+str1);
+
+            Console.WriteLine("\n7.Capitilize first letter in string");
+            Capitalize cap = new Capitalize();
+            Cap c1 = new Cap(cap.FirstCharToUpper);
+            c1(cap);
+
+            Console.ReadLine();
+        }
+    }
+}
