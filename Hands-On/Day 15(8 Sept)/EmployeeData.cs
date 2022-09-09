a018fb577a4ebcfe3fb5c6dacf2a7d97a1e328fe
Tejaswinideena <111355809+Tejaswinideena@users.noreply.github.com>
2022-09-09 13:19:16 +0530

    Add files via upload

---
+++ Hands-On/Day 15(8 Sept)/EmployeeData.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Handson
+{
+    public class EmployeeData
+    {
+        public delegate Dept del(Dept p);
+        public delegate void del1(List<Employee1> m);
+        public delegate List<Employee1> numb2(int i, List<Employee1> p);
+        public class Dept
+        {
+            public int DeptNo
+            {
+                get;
+                set;
+            }
+            public string DeptName
+            {
+                get;
+                set;
+            }
+            public string Location
+            {
+                get;
+                set;
+            }
+            public List<Employee1> list
+            {
+                get;
+                set;
+            }
+
+            List<Employee1> p = new List<Employee1>();
+            public Dept AddDept(Dept t)
+            { 
+                
+                if (p.Count <= 10)
+                {
+                    Employee1 m = new Employee1();
+                    Console.WriteLine("Enter the employee no");
+                    m.EmployeeNo = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Enter the name");
+                    m.Empname = Console.ReadLine();
+                    Console.WriteLine("Enter the dept no");
+                    m.DeptNo = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Enter the salary");
+                    m.Salary = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Enter the manager");
+                    m.Manager = Console.ReadLine();
+
+
+
+                    p.Add(m);
+                    t.list= p;
+                    return t;
+                }
+                else
+                {
+
+                    Console.WriteLine("You can't add more than 10 in a dept");
+                    return t;
+
+                }
+            }
+
+
+        }
+        public class Employee1:Dept
+        {
+
+            public int EmployeeNo
+            {
+                get;
+                set;
+            }
+            public string Empname
+            {
+                get;
+                set;
+            }
+            
+            public int Salary
+            {
+                get;
+                set;
+
+            }
+            public string Manager
+            {
+                get;
+                set;
+            }
+            public List<Employee1> Edit(int EmpId, List<Employee1> m)
+            {
+                Console.WriteLine("Edit Menu\n 1.Empname\n 2.Salary\n 3.Manager");
+                int a = Convert.ToInt32(Console.ReadLine());
+                switch (a)
+                {
+                    case 1:
+                        foreach (var item in m)
+                        {
+                            if (item.EmployeeNo == EmpId)
+                            {
+
+                                Console.WriteLine("Enter New name");
+                                item.Empname = Console.ReadLine();
+
+                            }
+                        }
+
+                        break;
+
+                    case 2:
+                        foreach (var item in m)
+                        {
+                            if (item.EmployeeNo == EmpId)
+                            {
+
+                                Console.WriteLine("Enter New salary");
+                                item.Salary = Convert.ToInt32(Console.ReadLine());
+
+                            }
+                        }
+
+                        break;
+                    case 3:
+                        foreach (var item in m)
+                        {
+                            if (item.EmployeeNo == EmpId)
+                            {
+
+                                Console.WriteLine("Enter New Manager");
+                                item.Manager = Console.ReadLine();
+                            }
+                        }
+
+                        break;
+                    default:
+                        break;
+                }
+
+                return m;
+            }
+            public List<Employee1> Delete(int id, List<Employee1> m)
+            {
+                m = m.Where(m1 => m1.EmployeeNo != id).ToList();
+                return m;
+
+            }
+            public void Displaybymgr(List<Employee1> m)
+            {
+                Console.WriteLine("Enter the Name of the Manager to know Employees who is working under him ");
+                string s = Console.ReadLine();
+                var mg = m.Where(m2 => m2.Manager == s).ToList();
+                if (mg.Count > 0)
+                {
+                    foreach (var item in mg)
+                        Console.WriteLine(item.Empname);
+                }
+                else
+                {
+                    Console.WriteLine("No Employee is working under him");
+                }
+            }
+            public void Display(List<Employee1> m)
+            {
+                foreach (var item in m)
+                {
+                    Console.WriteLine($"{item.EmployeeNo} {item.Empname} {item.DeptNo} {item.Salary} {item.Manager}\n");
+                }
+
+            }
+
+        }
+    }
+}
---
+++ Hands-On/Day 15(8 Sept)/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using static Handson.EmployeeData;
+
+namespace Sept8
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+        user:
+            Console.WriteLine("Enter the username");
+            string u = Console.ReadLine();
+            Console.WriteLine("Enter the password");
+            string p = Console.ReadLine();
+            var data = Tuple.Create<string, string>(u, p);
+            Predicate<Tuple<string, string>> s = (data1) => {
+                if (data1.Item1 == "FirstUser" && data1.Item2 == "hello123")
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            };
+            if (s(data))
+            {
+                List<Employee1> emplist = new List<Employee1>();
+            dis:
+                Console.WriteLine("Menu\n 1.Add\n 2.Delete\n 3.Edit");
+                int a= Convert.ToInt32(Console.ReadLine());
+
+                Dept D = new Dept();
+                switch(a)
+                {
+                    case 1:
+
+                    Console.WriteLine("Enter deptno");
+                    D.DeptNo = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("DeptName");
+                    D.DeptName = Console.ReadLine();
+                    Console.WriteLine("Location of department");
+                    D.Location = Console.ReadLine();
+                add:
+                    del d1 = new del(D.AddDept);
+                    D = d1(D);
+                    foreach (Employee1 e in D.list)
+                    {
+                        if (!emplist.Contains(e))
+                            emplist.Add(e);
+
+                    }
+                    Console.WriteLine("Enter Yes to add further,No to Display Details");
+                    string i = Console.ReadLine();
+                    if (i== "Yes")
+                    {
+                        goto add;
+                    }
+                    break;
+                    case 2:
+
+                    Employee1 emp = new Employee1();
+                    Console.WriteLine("Enter employee id to delete");
+                    int t = Convert.ToInt32(Console.ReadLine());
+                    emplist = emp.Delete(t, emplist);
+                    emp.Display(emplist);
+                    break;
+                    case 3:
+                    Console.WriteLine("Enter employee id to edit");
+                    int k = Convert.ToInt32(Console.ReadLine());
+                    Employee1 emp1 = new Employee1();
+                    emplist = emp1.Edit(k, emplist);
+                    emp1.Display(emplist);
+
+                    break;
+                }
+
+                Employee1 emp2 = new Employee1();
+                Console.WriteLine("Display all Employee names");
+                del1 d2 = new del1(emp2.Display);
+                d2(emplist);
+                del1 d3 = new del1(emp2.Displaybymgr);
+                d3(emplist);
+                Console.WriteLine("Enter Yes to Menu");
+                string pass = Console.ReadLine();
+                if (pass == "Yes")
+                {
+                    goto dis;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Try again");
+                goto user;
+            }
+            Console.ReadLine();
+        }
+    }
+}
