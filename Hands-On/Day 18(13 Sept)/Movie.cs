7c37042d0618f0682090a69a9f74800db927774f
Tejaswinideena <111355809+Tejaswinideena@users.noreply.github.com>
2022-09-14 11:45:41 +0530

    Section A (User Module)

---
+++ Hands-On/Day 18(13 Sept)/Movie.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Handson
+{
+    public class Movie
+    {
+        private string _name;
+        private string _lang;
+        private string _genre;
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+        public string Language
+        {
+            get { return _lang; }
+            set { _lang = value; }
+        }
+        public string Genre
+        {
+            get { return _genre; }
+            set { _genre = value; }
+        }
+        public int stock
+        {
+            get;
+            set;
+        }
+        public Movie(string name, string language, string genre, int stock)
+        {
+            _name = name;
+            _lang= language;
+            _genre = genre;
+            this.stock = stock;
+
+        }
+        public Movie()
+        {
+
+        }
+
+    }
+
+    public class User : Movie
+    {
+
+        public User()
+        {
+
+        }
+        public string Username
+        {
+            get;
+            set;
+        }
+        public string Type
+        {
+            get;
+            set;
+        }
+        public List<Movie> rented
+        {
+            get;
+            set;
+        }
+        public DateTime rentaldate
+        {
+            get;
+            set;
+        }
+        public DateTime releasingdate
+        {
+            get;
+            set;
+        }
+        public int Cost
+        {
+            get;
+            set;
+        }
+        public List<Movie> dbmovies = new List<Movie>();
+
+
+
+        public void initiate()
+        {
+            Movie m = new Movie("Vikram", "Tamil", "Action", 25);
+            dbmovies.Add(m);
+            Movie k = new Movie("KGF-2", "Kannada", "Action", 10);
+            dbmovies.Add(k);
+            Movie t = new Movie("SitaRamam", "Tamil", "Romance", 5);
+            dbmovies.Add(t);
+            Movie n = new Movie("Dia", "Kannada", "Romance", 50);
+            dbmovies.Add(n);
+            foreach (var item in dbmovies)
+            {
+                Console.WriteLine($"{item.Name}  {item.Language} {item.Genre} {item.stock}");
+            }
+
+
+
+        }
+
+        public void Addition(Movie p)
+        {
+            dbmovies.Add(p);
+        }
+        public void Removing(Movie p)
+        {
+            dbmovies.Remove(p);
+        }
+        public void Display()
+        {
+            foreach (var item in dbmovies)
+            {
+                Console.WriteLine($"{item.Name}  {item.Language} {item.Genre} {item.stock}");
+            }
+        }
+
+
+
+
+        public List<Movie> adding(List<Movie> rented1, string Type1)
+        {
+            string s = Console.ReadLine();
+            if (Type1.Equals("Gold") && rented1.Count > 3)
+            {
+                Console.WriteLine("You cannot add");
+                return rented1;
+            }
+            else if (Type1.Equals("Silver") && rented1.Count > 1)
+            {
+                Console.WriteLine("You cannot add");
+                return rented1;
+            }
+            else if (Type1.Equals("Platinum") && rented1.Count > 5)
+            {
+                Console.WriteLine("You cannot add");
+                return rented1;
+            }
+            else
+            {
+
+                var pi = dbmovies.Where(p => p.Name == s).ToList();
+                if (pi != null)
+                {
+                    foreach (Movie item in pi)
+                    {
+
+                        Console.WriteLine(item.Name);
+
+                        dbmovies.Remove(item);
+                        item.stock = item.stock - 1;
+                        rented1.Add(item);
+                        dbmovies.Add(item);
+
+
+                    }
+                    return rented1;
+                }
+                else
+                {
+                    Console.WriteLine("Asked Movie is not available");
+                    return rented1;
+                }
+
+
+            }
+
+
+        }
+        public List<Movie> releasing(List<Movie> rented1)
+        {
+
+            string cp = Console.ReadLine();
+            var po = dbmovies.Where(p => p.Name == cp).ToList();
+
+            foreach (var item in po)
+            {
+                dbmovies.Remove(item);
+                item.stock = item.stock + 1;
+                rented1.Remove(item);
+                dbmovies.Add(item);
+
+            }
+            return rented1;
+
+        }
+        public double TotalCost()
+        {
+            TimeSpan ts = releasingdate - rentaldate;
+            int d = ts.Days;
+            double k = 0.18 * Cost;
+            double a = d * 0.01 * Cost;
+            double b= k + a+ Cost;
+            Console.WriteLine($"GST is {k}");
+            Console.WriteLine($"Bluray cost is {a}");
+            Console.WriteLine("Total cost is" + " " + b);
+            return b;
+        }
+    }
+}
---
+++ Hands-On/Day 18(13 Sept)/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using Handson;
+
+namespace Sept13
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+        user:
+            Console.WriteLine("Enter the username");
+            string p = Console.ReadLine();
+            Console.WriteLine("Enter the password");
+            string ps = Console.ReadLine();
+            Tuple<string, string> t = Tuple.Create<string, string>(p, ps);
+
+            Predicate<Tuple<string, string>> pt = (i) =>
+            {
+                if (i.Item1 == "user1" && i.Item2 == "password")
+                {
+
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+
+            };
+            bool b = pt.Invoke(t);
+            if (b)
+            {
+                Console.WriteLine("Logged in successfully");
+                User u = new User();
+                Console.WriteLine("Enter user");
+                p = Console.ReadLine();
+                u.Username = p;
+                Console.WriteLine("Enter type");
+                u.Type = Console.ReadLine();
+                u.initiate();
+                List<Movie> s = new List<Movie>();
+                double total = 0;
+                int Count1 = 0;
+            add:
+                Console.WriteLine("Adding");
+                s = u.adding(s, u.Type);
+                Count1++;
+                u.rented = s;
+                if (s.Count == Count1)
+                {
+                    Console.WriteLine("Enter the rental year");
+                    int yr = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Enter the rental month");
+                    int mon = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Enter the rental date");
+                    int dt = Convert.ToInt32(Console.ReadLine());
+                    DateTime top = new DateTime(yr, mon, dt);
+                    u.rentaldate = top;
+                    Console.WriteLine("Enter the releasing year");
+                    int ryr= Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Enter the releasing month");
+                    int rmon = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Enter the releasing date");
+                    int rdt= Convert.ToInt32(Console.ReadLine());
+                    DateTime pot = new DateTime(ryr, rmon, rdt);
+                    u.releasingdate = pot;
+                    Console.WriteLine("Enter the cost");
+                    u.Cost = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Total cost for this movie", +u.TotalCost());
+                    total += u.TotalCost();
+                    Console.WriteLine("Enter Yes to further rental");
+                    string str = Console.ReadLine();
+                    if (str == "Yes")
+                    {
+                        goto add;
+                    }
+                }
+                Console.WriteLine("Stocks after rented");
+
+                foreach (var item in u.dbmovies)
+                {
+                    Console.WriteLine($"{item.Name} {item.stock}");
+                }
+
+                s = u.releasing(s);
+                u.rented = s;
+                Console.WriteLine("Total cost" + total);
+                foreach (var item in u.rented)
+                {
+                    Console.WriteLine(item.Name);
+                }
+
+                Console.ReadLine();
+
+            }
+            else
+            {
+                Console.WriteLine("Try again");
+                goto user;
+            }
+        }
+    }
+}
