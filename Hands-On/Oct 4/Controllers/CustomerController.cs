ead60472a4f0205c3906b4af9f241de0ca992879
Tejaswinideena <111355809+Tejaswinideena@users.noreply.github.com>
2022-10-10 17:11:52 +0530

    Add files via upload

---
+++ Hands-On/Oct 4/Controllers/CustomerController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Oct_4.Controllers
+{
+    public class CustomerController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult SearchCustomer(int id)
+        {
+            return Content("Tried to find the Customer id= " + id);
+        }
+        public ActionResult AddCustomer()
+        {
+            return View();
+        }
+
+        public ActionResult GoToGoogle()
+        {
+
+            return Redirect("https://www.google.com");
+        }
+
+
+        public ActionResult DeleteCustomer()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult DeleteCustomer(int id)
+        {
+
+            return View();
+        }
+
+        public ActionResult GoToIndexPageofHomeController()
+        {
+            return RedirectToAction("Contact", "Home");
+        }
+        public ActionResult ShowCustomer(int id, string pname)
+        {
+
+            return Content("ID and Name:" + id + pname);
+        }
+    }
+}
---
+++ Hands-On/Oct 4/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Oct_4.MvcApplication" Language="C#" %>
