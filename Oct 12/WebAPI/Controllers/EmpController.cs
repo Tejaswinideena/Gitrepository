c11644219aab9df889dfdd9d309b662163e57b37
Tejaswinideena <111355809+Tejaswinideena@users.noreply.github.com>
2022-10-12 18:25:22 +0530

    Add

---
+++ Oct 12/WebAPI/Controllers/EmpController.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebAPI.Models;
+using HelperLibrary;
+using BLL;
+namespace WebAPI.Controllers
+{
+    [RoutePrefix("MEmp")]
+    public class EmpController : ApiController
+    {
+        // GET api/<controller>
+        EmployeeHelper obj = null;
+        public EmpController()
+        {
+            obj = new EmployeeHelper();
+        }
+
+        [Route("GetAllEmps")]
+        public List<Employees> GetEmpList()
+        {
+            //return new string[] { "value1", "value2" };
+
+            List<Employee_BAL> empbal = new List<Employee_BAL>(); empbal = obj.EmployeeList();
+            List<Employees> emps = new List<Employees>();
+            foreach (var item in empbal)
+            {
+                //Employees emp = new Employees();
+                emps.Add(new Employees { EmployeeID = item.EmployeeID, FirstName = item.FirstName, LastName = item.LastName, BirthDate = item.BirthDate, Title = item.Title });
+            }
+            return emps;
+
+        }
+
+        [Route("~/FindEmp")]
+        public Employees GetEmployeeDetails()
+        {
+            return new Employees
+            {
+
+
+                EmployeeID = 1,
+                LastName = "Davolio",
+                FirstName = "Nancy",
+                Title = "Sales Representative",
+                BirthDate = new DateTime(1948, 08, 12)
+            };
+        }
+        // GET api/<controller>/5
+        [Route("~/FindEmp/{id}")]
+        //  [Route("FindEmp/{id:int:min(1)}")]
+        //[Route("FindEmp/{id:int?}")]
+        public Employees GetEmpByID(int id = 1)
+        {
+            Employee_BAL empbal = new Employee_BAL();
+            empbal = obj.LocateEmployee(id);
+            Employees emp = new Employees();
+            emp.EmployeeID = empbal.EmployeeID;
+            emp.FirstName = empbal.FirstName;
+            emp.LastName = empbal.LastName;
+            emp.Title = empbal.Title;
+            emp.BirthDate = empbal.BirthDate;
+            return emp;
+            //return "value";
+        }
+
+        // POST api/<controller>
+        public HttpResponseMessage PostProduct([FromBody] Employees empdata)
+        {
+            Employee_BAL empbal = new Employee_BAL();
+            empbal.EmployeeID = empdata.EmployeeID;
+            empbal.FirstName = empdata.FirstName;
+            empbal.LastName = empdata.LastName;
+            empbal.Title = empdata.Title;
+            empbal.BirthDate = empdata.BirthDate;
+
+
+            bool ans = obj.AddNewEmployee(empbal);
+            if (ans)
+            {
+                return Request.CreateResponse(HttpStatusCode.OK);
+            }
+            else
+            {
+                return Request.CreateResponse(HttpStatusCode.NotAcceptable);
+            }
+
+        }
+
+        // PUT api/<controller>/5
+        public HttpResponseMessage PutProduct(int id, [FromBody] Employees empdata)
+        {
+
+            Employee_BAL empbal = new Employee_BAL();
+            empbal.EmployeeID = empdata.EmployeeID;
+            empbal.FirstName = empdata.FirstName;
+            empbal.LastName = empdata.LastName;
+            empbal.Title = empdata.Title;
+            empbal.BirthDate = empdata.BirthDate;
+
+            bool ans = obj.EditEmployeeData(id, empbal);
+            if (ans)
+            {
+                return Request.CreateResponse(HttpStatusCode.OK);
+            }
+            else
+            {
+                return Request.CreateResponse(HttpStatusCode.NotAcceptable);
+            }
+        }
+
+        // DELETE api/<controller>/5
+        public HttpResponseMessage DeleteProduct(int id)
+        {
+            bool ans = obj.RemoveEmployeeData(id);
+            if (ans)
+            {
+                return Request.CreateResponse(HttpStatusCode.OK);
+            }
+            else
+            {
+                return Request.CreateResponse(HttpStatusCode.NotAcceptable);
+            }
+
+        }
+    }
+}
