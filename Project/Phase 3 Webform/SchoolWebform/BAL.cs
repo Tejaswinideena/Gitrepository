308b5227e6cd72d55857b8b3facc3fe2c1bac434
Tejaswinideena <111355809+Tejaswinideena@users.noreply.github.com>
2022-10-14 16:30:34 +0530

    Add

---
+++ Project/Phase 3 Webform/SchoolWebform/BAL.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SchoolWebform
+{
+    public class ClassBAL
+    {
+        public int ClassID { get; set; }
+        public string ClassName { get; set; }
+    }
+    public class StudentsBAL
+    {
+        public int StudentID { get; set; }
+        public string StudentName { get; set; }
+        public int ClassID { get; set; }
+    }
+    public class SubjectsBAL
+    {
+        public int SubjectID { get; set; }
+        public string SubjectName { get; set; }
+        public int ClassID { get; set; }
+    }
+}
+
+
---
+++ Project/Phase 3 Webform/SchoolWebform/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("SchoolWebform")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SchoolWebform")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("291d3191-7628-4296-9cea-85246c7d0ab4")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
