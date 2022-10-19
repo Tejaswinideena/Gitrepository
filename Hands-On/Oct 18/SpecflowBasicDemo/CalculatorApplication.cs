d10d9aa2a92e7cab36d0dbb99d9bb0d9751133d9
Tejaswinideena <111355809+Tejaswinideena@users.noreply.github.com>
2022-10-19 18:18:31 +0530

    Add

---
+++ Hands-On/Oct 18/SpecflowBasicDemo/CalculatorApplication.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpecflowBasicDemo
+{
+    public class CalculatorApplication
+    {
+        public int Add(int i,int j)
+        {
+            return i + j;
+        }
+        public int Substract(int i, int j)
+        {
+            return i - j;
+        }
+        public int Multiply(int i, int j)
+        {
+            return i * j;
+        }
+        public int Divide(int i, int j)
+        {
+            return j != 0 ? i / j: 0;
+        }
+        public int Sqrt(double i)
+        {
+            return i != 0 ? (int)Math.Sqrt(i) : 0;
+        }
+    }
+}
