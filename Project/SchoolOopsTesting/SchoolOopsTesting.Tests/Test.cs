43bd148dba9b488b6e157b8b98f03b85c4cc551e
Tejaswinideena <111355809+Tejaswinideena@users.noreply.github.com>
2022-11-02 14:23:42 +0530

    OOPS Testing Phase 4 Practice Project

---
+++ Project/SchoolOopsTesting/SchoolOopsTesting.Tests/Test.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SchoolOopsTesting;
+using NUnit.Framework;
+
+
+namespace SchoolOopsTesting.Tests
+{
+    [TestFixture]
+    public class Test
+    {
+        [TestCase]
+        public void Test1()
+        {
+            student s = new student();
+            s.student_id = 1;
+            s.student_name = "ram";
+            Assert.AreEqual(s.student_id, 1);
+        }
+        [TestCase]
+        public void Test2()
+        {
+            subject s = new subject();
+            s.subject_id = 101;
+            s.subject_name = "physics";
+            s.subject_marks = 95;
+            Assert.AreEqual(s.subject_marks, 95);
+        }
+        [TestCase]
+        public void Test3()
+        {
+            teacher s = new teacher();
+            s.teacher_id = 12;
+            s.teacher_name = "raj";
+            Assert.AreEqual(s.teacher_name, "raj");
+        }
+
+    }
+}
+
---
+++ Project/SchoolOopsTesting/SchoolOopsTesting/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolOopsTesting
+{
+    public class student
+    {
+        public int student_id { get; set; }
+        public string student_name { get; set; }
+    }
+    public class subject
+    {
+        public int subject_id { get; set; }
+        public string subject_name { get; set; }
+        public int subject_marks { get; set; }
+    }
+    public class teacher
+    {
+        public int teacher_id { get; set; }
+        public string teacher_name { get; set; }
+    }
+}
